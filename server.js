77418a11f5cd63de031268c5e479526ad374813f
Mineth Perera <149650637+minethz@users.noreply.github.com>
2026-05-01 21:42:23 +0530

    sa

--- server.js
+++ server.js
@@ -721,7 +721,7 @@ app.get("/", (req, res) => {
 
 
 // ------------------ START SERVER ------------------
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
